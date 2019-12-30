f6da86d789288fd08353f1888ee192c218cf7466
Joseph Merkel <joseph_merkel@icloud.com>
2019-12-29 23:37:13 -0800

    fixed VScode & compared files

--- apps/scraping.py
+++ apps/scraping.py
@@ -2,10 +2,15 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
+import datetime as dt
+
+#.find() = Find first class & Attribute
+#.find_all() = find ALL tags & attributes
+#.get_text() = only retrieve text, no HTML
 
 def scrape_all():
     # Initiate headless driver for deployment
-    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    browser = Browser("chrome", executable_path='/usr/local/bin/chromedriver', headless=True)
     news_title, news_paragraph = mars_news(browser)
     # Run all scraping functions and store results in dictionary
     data = {
@@ -15,10 +20,7 @@ def scrape_all():
         "facts": mars_facts(),
         "last_modified": dt.datetime.now()
     }
-
-# Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path)
+    return data
 
 def mars_news(browser):
     # Visit the mars nasa news site
@@ -32,16 +34,8 @@ def mars_news(browser):
 
     try:
         slide_elem = news_soup.select_one('ul.item_list li.slide')
-        #slide_elem.find("div", class_='content_title')
-
         # Use the parent element to find the first `a` tag and save it as `news_title`
         news_title = slide_elem.find("div", class_='content_title').get_text()
-        #news_title
-
-        #.find() = Find first class & Attribute
-        #.find_all() = find ALL tags & attributes
-        #.get_text() = only retrieve text, no HTML
-
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
         #news_p
@@ -52,36 +46,34 @@ def mars_news(browser):
 
 # ### Featured Images
 
-# Visit URL
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
 
-
-# Find and click the full image button
-full_image_elem = browser.find_by_id('full_image')
-full_image_elem.click()
-
-# Find the more info button and click that
-browser.is_element_present_by_text('more info', wait_time=1)
-more_info_elem = browser.find_link_by_partial_text('more info')
-more_info_elem.click()
-
-# Parse the resulting html with soup
-html = browser.html
-img_soup = BeautifulSoup(html, 'html.parser')
 
 def featured_image(browser):
+    # Visit URL
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem.click()
+
+    # Find the more info button and click that
+    browser.is_element_present_by_text('more info', wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text('more info')
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
     try:
         # Find the relative image url
         img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-        #img_url_rel
     
     except AttributeError:
         return None
     
     # Use the base URL to create an absolute URL
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-    
     return img_url
 
 
@@ -89,15 +81,13 @@ def mars_facts():
     try:
         # Parse first [0] table that is found
         df = pd.read_html('http://space-facts.com/mars/')[0]
-
     except BaseException:
         return None
 
-    # Assign column names
-    df.columns=['description', 'value']
-    # Specify index as description
-    df.set_index('description', inplace=True)
-
+    df.columns=['description', 'value']     # Assign column names
+    df.set_index('description', inplace=True)     # Specify index as description
     return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
