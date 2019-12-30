dc87ff8027c1b5beab97bbdb49969538852fd6f3
Joseph Merkel <joseph_merkel@icloud.com>
2019-12-29 22:50:08 -0800

    Pre-Compare

--- scraping.py
+++ scraping.py
@@ -3,33 +3,52 @@ from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
 # Set the executable path and initialize the chrome browser in splinter
 executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
 browser = Browser('chrome', **executable_path)
 
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+def mars_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-html = browser.html
-news_soup = BeautifulSoup(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide')
+    html = browser.html
+    news_soup = BeautifulSoup(html, 'html.parser')
 
-slide_elem.find("div", class_='content_title')
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        #slide_elem.find("div", class_='content_title')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+        #news_title
 
-#.find() = Find first class & Attribute
-#.find_all() = find ALL tags & attributes
-#.get_text() = only retrieve text, no HTML
+        #.find() = Find first class & Attribute
+        #.find_all() = find ALL tags & attributes
+        #.get_text() = only retrieve text, no HTML
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-news_p
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+        #news_p
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
 
 # ### Featured Images
 
@@ -51,22 +70,34 @@ more_info_elem.click()
 html = browser.html
 img_soup = BeautifulSoup(html, 'html.parser')
 
-# Find the relative image url
-img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-img_url_rel
+def featured_image(browser):
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+        #img_url_rel
+    
+    except AttributeError:
+        return None
+    
+    # Use the base URL to create an absolute URL
+    img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    
+    return img_url
 
-# Use the base URL to create an absolute URL
-img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-img_url
 
-# Parse first [0] table that is found
-df = pd.read_html('http://space-facts.com/mars/')[0]
-# Assign column names
-df.columns=['description', 'value']
-# Specify index as description
-df.set_index('description', inplace=True)
-df
+def mars_facts():
+    try:
+        # Parse first [0] table that is found
+        df = pd.read_html('http://space-facts.com/mars/')[0]
 
-df.to_html()
+    except BaseException:
+        return None
+
+    # Assign column names
+    df.columns=['description', 'value']
+    # Specify index as description
+    df.set_index('description', inplace=True)
+
+    return df.to_html()
 
 browser.quit()
